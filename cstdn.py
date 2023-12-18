ea5c17400fc164afd9342b0fb2b55fa1678e77c7
lukeamyers <lam7027@psu.edu>
2023-12-18 14:07:37 -0500

    just comments

--- cstdn.py
+++ cstdn.py
@@ -90,6 +90,10 @@ def extract_simple_mag_data(ion_list, outcar_path='OUTCAR'):
     simple_data.reset_index(drop=True, inplace=True)
     return simple_data
 
+"""
+df is a data frame with columns ['config', '# of ion', 'vol', 'tot']
+not sure what happens if you don't include config, might still work.
+"""
 def plot_mv(df, show_fig=True):
     fig = px.line(df,
                     x='vol',
