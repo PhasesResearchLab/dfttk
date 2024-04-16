95b818f54d4db2607a84241b23b542423cced474
nhew1994 <nigelhew94@gmail.com>
2024-04-16 12:00:05 -0400

    Added the option in def three_step_relaxation and def ev_curves_series to override the default settings. Updated examples in ev_curves_example.py and three_step_relaxation_example.py to show how to use the new option.

--- scripts/three_step_relaxation_example.py
+++ scripts/three_step_relaxation_example.py
@@ -4,8 +4,8 @@ import numpy as np
 from custodian.vasp.handlers import VaspErrorHandler
 
 # Replace the path below with your own path to workflows.py
-sys.path.append('/storage/home/lam7027/work/bin/vasp-job-automation')
-import src.workflows as workflows
+#sys.path.append('/storage/home/lam7027/work/bin/vasp-job-automation')
+import workflows as workflows
 
 # Specify custodian handlers
 subset = list(VaspErrorHandler.error_msgs.keys())
@@ -14,8 +14,20 @@ subset.remove("brmix")
 subset.remove("zbrent")
 handlers = [VaspErrorHandler(errors_subset_to_catch=subset)]
 
-# Specify VASP command
 vasp_cmd = ["srun", "vasp_std"]
 
-workflows.three_step_relaxation(os.getcwd(), vasp_cmd, handlers)
+settings_override_2relax = [
+            {"file": "CONTCAR", "action": {"_file_copy": {"dest": "POSCAR"}}}
+        ]
+settings_override_3static=[
+    {
+        "dict": "INCAR",
+        "action": {
+            "_set": {"ALGO": "Normal", "IBRION": -1, "NSW": 0, "ISMEAR": 0, "SIGMA": 0.05}
+        },
+    },
+    {"file": "CONTCAR", "action": {"_file_copy": {"dest": "POSCAR"}}},
+]
+        
+workflows.three_step_relaxation(os.getcwd(), vasp_cmd, handlers,default_settings=True)
 
