7720e823c6e829b89782786ed27eb395df05fce9
lukeamyers <lam7027@psu.edu>
2024-04-30 10:51:40 -0400

    Added eos_fit.plot_energy_difference() function. It utililizes the plot_ev() function. There is currently only works if to subtract the eneries for a 'bcc' config. will fix

--- dfttk/eos_fit.py
+++ dfttk/eos_fit.py
@@ -1282,6 +1282,31 @@ def plot_ev(
         fig.show()
     return fig
 
+def plot_energy_difference(df, per_atom=False, show_fig=True, convert_to_mev=False):
+    df_list = []
+    for config in df['config'].unique():
+        df_list.append(df[df['config'] == config].reset_index(drop=True))
+        if config == 'bcc':
+            df_bcc = df[df['config'] == config].reset_index(drop=True)
+
+    # Subtract the 'energy' columns
+    for df_el in df_list:
+        df_el['energy'] -= df_bcc['energy']
+        
+    energy_difference_df = pd.concat(df_list)
+    
+    # convert to meV
+    if convert_to_mev == True:
+        energy_difference_df['energy'] *= 1000
+    
+    # plot energy difference vs volume
+    fig = plot_ev(energy_difference_df, eos_fitting=None, per_atom=per_atom, show_fig=False)
+    if convert_to_mev:
+        fig.update_layout(title="Energy Difference vs Volume", yaxis_title="Energy Difference (meV)")
+    else:
+        fig.update_layout(title="Energy Difference vs Volume", yaxis_title="Energy Difference (eV)")
+    if show_fig:
+        fig.show()    
 
 def plot_config_energy(
     df, number_of_lowest_configs=5, show_fig=True, xmax=None, ymax=None
