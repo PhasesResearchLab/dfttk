7035bc384eb6e2cc09588eb8c4e4566b4977ae87
lukeamyers <lam7027@psu.edu>
2024-07-16 12:54:16 -0400

    added extract_convergence_data function

--- dfttk/aggregate_extraction.py
+++ dfttk/aggregate_extraction.py
@@ -12,12 +12,14 @@ import pandas as pd
 # Local application/library specific imports
 from pymatgen.core.structure import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.io.vasp.inputs import Incar
 
 # DFTTK imports
 from dfttk.data_extraction import (
     extract_volume,
     extract_energy,
     extract_tot_mag_data,
+    extract_kpoints,
 )
 from dfttk.magnetism import determine_magnetic_ordering
 
@@ -149,4 +151,65 @@ def recursive_extract_configuration_data(
         except Exception as e:
             print(f"Error in {config_dir}: {e}")
     df = pd.concat(df_list, ignore_index=True)
+    return df
+
+def extract_convergence_data(path: str) -> pd.DataFrame:
+    """Calculates the energy convergence with respect to ENCUT and plots the results.
+
+    Args:
+        path (str): path to the folder containing the VASP input files
+        plot (bool, optional): If True, plots the energy per atom vs. ENCUT. Defaults to True.
+
+    Returns:
+        pd.DataFrame: a pandas dataframe containing the ENCUT, energy, number of atoms, energy per atom, and difference in energy per atom.
+        go.Figure: a plotly figure of the energy per atom vs. ENCUT.
+    """
+
+    OSZICAR_files = [
+        file
+        for file in os.listdir(path)
+        if os.path.isfile(os.path.join(path, file))
+        and file.startswith("OSZICAR")
+    ]
+    
+    conv_items = sorted([int(file.split(".")[1]) for file in OSZICAR_files])
+    
+    encut_list = []
+    energy_list = []
+    number_of_atoms_list = []
+    kpoints_list = []
+    for item in conv_items:
+        oszicar_path = os.path.join(path, f"OSZICAR.{item}")
+        outcar_path = os.path.join(path, f"OUTCAR.{item}")
+        incar_path = os.path.join(path, f"INCAR.{item}")
+        poscar_path = os.path.join(path, f"POSCAR.{item}")
+        incar = Incar.from_file(incar_path)
+        struct = Structure.from_file(poscar_path)
+        
+        encut_list.append(incar.get("ENCUT", None))
+        energy_list.append(extract_energy(oszicar_path))
+        number_of_atoms_list.append(len(struct.sites))
+        kpoints_list.append(extract_kpoints(outcar_path))
+
+    energy_per_atom_list = [
+        energy / num_atoms 
+        for energy, num_atoms in zip(energy_list, number_of_atoms_list)
+    ]
+    
+    difference_meV_per_atom_list = [
+        (energy_per_atom_list[i] - energy_per_atom_list[i - 1]) * 1000
+        for i in range(1, len(energy_per_atom_list))
+    ]
+    difference_meV_per_atom_list.insert(0, float("nan"))
+    
+    df = pd.DataFrame(
+        {
+            "ENCUT": encut_list,
+            "energy": energy_list,
+            "number_of_atoms": number_of_atoms_list,
+            "energy_per_atom": energy_per_atom_list,
+            "difference_meV_per_atom": difference_meV_per_atom_list,
+        }
+    )
+    
     return df
