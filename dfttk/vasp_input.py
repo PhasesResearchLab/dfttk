a8939331541563293df3c705b4fa07e54b06d0b1
nhew1994 <nigelhew94@gmail.com>
2024-10-25 11:16:52 -0400

    Added docstrings and removed redundant function.

--- dfttk/vasp_input.py
+++ dfttk/vasp_input.py
@@ -15,21 +15,39 @@ import yaml
 from pymatgen.core.structure import Structure
 from pymatgen.io.vasp.inputs import Incar, Kpoints, Poscar, Potcar
 
-with open("potcar_dict.yaml", "r") as file:
+yaml_dir = os.path.dirname(__file__)
+potcar_dict_path = os.path.join(yaml_dir, "potcar_dict.yaml")
+magmom_dict_path = os.path.join(yaml_dir, "magmom_dict.yaml")
+with open(potcar_dict_path, "r") as file:
     POTCAR_DICT = yaml.safe_load(file)
 
-with open("magmom_dict.yaml", "r") as file:
+with open(magmom_dict_path, "r") as file:
     MAGMOM_DICT = yaml.safe_load(file)
+
 
 def base_set(
     path: str,
     material_type: str,
     encut: int = 520,
     kppa: int = 4000,
-    magnetic: bool = False,
+    magmom_fm: bool = False,
     potcar_functional: str = "PBE_54",
     incar_functional: str = "PBE",
-) -> None:
+) -> dict:
+    """Base settings for VASP input files.
+
+    Args:
+        path (str): path to folder containing POSCAR file
+        material_type (str): metal or non_metal
+        encut (int, optional): ENCUT value. Defaults to 520.
+        kppa (int, optional): k-point mesh per reciprocal atom. Defaults to 4000.
+        magmom_fm (bool, optional): include MAGMOM tag in the INCAR file. Defaults to False.
+        potcar_functional (str, optional): type of POTCAR file. Defaults to "PBE_54".
+        incar_functional (str, optional): GGA or METAGGA tag in the INCAR file. Defaults to "PBE".
+
+    Returns:
+        dict: INCAR settings
+    """
 
     poscar_path = os.path.join(path, "POSCAR")
     struct = Structure.from_file(poscar_path)
@@ -70,8 +88,8 @@ def base_set(
     }
 
     incar_settings.update(material_settings.get(material_type, {}))
-    
-    if magnetic:
+
+    if magmom_fm:
         magmom = [MAGMOM_DICT[site.specie.symbol] for site in struct.sites]
         incar_settings.update({"MAGMOM": magmom, "ISPIN": 2, "LORBIT": 11})
 
@@ -83,14 +101,26 @@ def volume_relax_set(
     material_type: str,
     encut: int = 520,
     kppa: int = 4000,
-    magnetic: bool = False,
+    magmom_fm: bool = False,
     potcar_functional: str = "PBE_54",
     incar_functional: str = "PBE",
     other_settings: dict = {},
 ) -> None:
+    """Prepare VASP input files for volume relaxation.
+
+    Args:
+        path (str): path to folder containing POSCAR file
+        material_type (str): metal or non_metal
+        encut (int, optional): ENCUT value. Defaults to 520.
+        kppa (int, optional): k-point mesh per reciprocal atom. Defaults to 4000.
+        magmom_fm (bool, optional): include MAGMOM tag in the INCAR file. Defaults to False.
+        potcar_functional (str, optional): type of POTCAR file. Defaults to "PBE_54".
+        incar_functional (str, optional): GGA or METAGGA tag in the INCAR file. Defaults to "PBE".
+        other_settings (dict, optional): include other INCAR tags. Defaults to {}.
+    """
 
     incar_settings = base_set(
-        path, material_type, encut, kppa, magnetic, potcar_functional, incar_functional
+        path, material_type, encut, kppa, magmom_fm, potcar_functional, incar_functional
     )
     incar_settings.update(
         {
@@ -102,7 +132,7 @@ def volume_relax_set(
     )
     incar_settings.update(other_settings)
     incar = Incar(incar_settings)
-    
+
     incar.write_file(os.path.join(path, "INCAR"))
 
 
@@ -110,15 +140,26 @@ def conv_set(
     path: str,
     encut: int = 520,
     kppa: int = 4000,
-    magnetic: bool = False,
+    magmom_fm: bool = False,
     potcar_functional: str = "PBE_54",
     incar_functional: str = "PBE",
     other_settings: dict = {},
 ) -> None:
+    """Prepare VASP input files for convergence tests.
+
+    Args:
+        path (str): path to folder containing POSCAR file
+        encut (int, optional): ENCUT value. Defaults to 520.
+        kppa (int, optional): k-point mesh per reciprocal atom. Defaults to 4000.
+        magmom_fm (bool, optional): include MAGMOM tag in the INCAR file. Defaults to False.
+        potcar_functional (str, optional): type of POTCAR file. Defaults to "PBE_54".
+        incar_functional (str, optional): GGA or METAGGA tag in the INCAR file. Defaults to "PBE".
+        other_settings (dict, optional): include other INCAR tags. Defaults to {}.
+    """
 
     material_type = "metal"  # Will be overwridden by ISMEAR = -5
     incar_settings = base_set(
-        path, material_type, encut, kppa, magnetic, potcar_functional, incar_functional
+        path, material_type, encut, kppa, magmom_fm, potcar_functional, incar_functional
     )
     incar_settings.update(
         {
@@ -144,6 +185,17 @@ def ev_curve_set(
     incar_functional: str = "PBE",
     other_settings: dict = {},
 ) -> None:
+    """Prepare VASP input files for energy-volume curve calculations.
+
+    Args:
+        path (str): path to folder containing POSCAR file
+        material_type (str): metal or non_metal
+        encut (int, optional): ENCUT value. Defaults to 520.
+        kppa (int, optional): k-point mesh per reciprocal atom. Defaults to 4000.
+        potcar_functional (str, optional): type of POTCAR file. Defaults to "PBE_54".
+        incar_functional (str, optional): GGA or METAGGA tag in the INCAR file. Defaults to "PBE".
+        other_settings (dict, optional): include other INCAR tags. Defaults to {}.
+    """
 
     incar_settings = base_set(
         path, material_type, encut, kppa, potcar_functional, incar_functional
@@ -160,30 +212,3 @@ def ev_curve_set(
 
     incar = Incar(incar_settings)
     incar.write_file(os.path.join(path, "INCAR"))
-
-
-def perturb_structure(
-    path, displacement_magnitude, atoms_to_perturb, number_of_perturbations
-):
-    parent_dir = os.path.dirname(path)
-
-    structure = Poscar.from_file(path).structure
-
-    for i in range(number_of_perturbations):
-        structure_copy = structure.copy()
-
-        # Same magnitude, but different vector components for each perturbation on each atom
-        for atom in atoms_to_perturb:
-            random_vector = np.random.rand(3) - 0.5
-            random_vector_magnitude = np.linalg.norm(random_vector)
-            normalized_random_vector = random_vector / random_vector_magnitude
-            displacement_vector = normalized_random_vector * displacement_magnitude
-            structure_copy[atom].coords = (
-                structure_copy[atom].coords + displacement_vector
-            )
-
-        perturbed_structure = Poscar(structure_copy)
-        os.makedirs(os.path.join(parent_dir, f"perturb_{i}"))
-        perturbed_structure.write_file(
-            os.path.join(parent_dir, f"perturb_{i}", "POSCAR")
-        )
