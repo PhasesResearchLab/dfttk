bbf9caa37cb12c02eeb2d8bf286ed01ab0d5523c
nhew1994 <nigelhew94@gmail.com>
2025-01-07 14:48:50 -0500

    Tests for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 # Standard library imports
 import os
+import math
 import json
 
 # Third-party library imports
@@ -179,6 +180,14 @@ def _convert_pbc_lists_to_tuples(data):
     data["lattice"]["pbc"] = tuple(data["lattice"]["pbc"])
     return data
 
+# Review
+def _assert_dict_almost_equal(dict1, dict2, tol=1e-6):
+    for key in dict1:
+        if isinstance(dict1[key], float) and isinstance(dict2[key], float):
+            assert math.isclose(dict1[key], dict2[key], rel_tol=tol), f"Expected {dict2[key]} for key '{key}', but got {dict1[key]}"
+        else:
+            assert dict1[key] == dict2[key], f"Expected {dict2[key]} for key '{key}', but got {dict1[key]}"
+
 
 def test_process_ev_curves():
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -246,6 +255,8 @@ def test_process_ev_curves():
     assert (
         config_Al.ev_curves.mag_data == []
     ), f"Expected [], but got {config_Al.ev_curves.mag_data}"
+    
+    '''
     assert config_Al.ev_curves.eos_parameters == {
         "eos_name": "BM4",
         "a": 10.115871836386141,
@@ -259,6 +270,21 @@ def test_process_ev_curves():
         "BP": 4.612739661291564,
         "B2P": -0.06258448064264342,
     }, f"Expected {repr({'eos_name': 'BM4', 'a': 10.115871836386141, 'b': -639.1561876497228, 'c': 781.9858370675397, 'd': 48419.8400405475, 'e': 0.0, 'V0': 66.10191547034127, 'E0': -14.972775074363833, 'B': 77.92792067011315, 'BP': 4.612739661291564, 'B2P': -0.06258448064264342})}, but got {repr(config_Al.ev_curves.eos_parameters)}"
+    '''
+    expected_eos_parameters = {
+        "eos_name": "BM4",
+        "a": 10.115871836386141,
+        "b": -639.1561876497228,
+        "c": 781.9858370675397,
+        "d": 48419.8400405475,
+        "e": 0.0,
+        "V0": 66.10191547034127,
+        "E0": -14.972775074363833,
+        "B": 77.92792067011315,
+        "BP": 4.612739661291564,
+        "B2P": -0.06258448064264342,
+    }
+    _assert_dict_almost_equal(config_Al.ev_curves.eos_parameters, expected_eos_parameters)
     
     actual_relaxed_structures = [
         structure.as_dict() for structure in config_Al.ev_curves.relaxed_structures
