4708515c2d85ee47bf09055b7e1dce6c9c61847d
nhew1994 <nigelhew94@gmail.com>
2025-03-03 16:45:45 -0500

    Tests for magnetism module

--- dfttk/magnetism.py
+++ dfttk/magnetism.py
@@ -13,25 +13,22 @@ from pymatgen.core.structure import Structure
 from dfttk.data_extraction import extract_tot_mag_data
 
 
-# TODO: While this does work, we also want the option to determine the magnetic ordering based on only certain elements.
-# For example, for Fe3Pt, we may want to determine the magnetic ordering based only on the Fe atoms.
-# In principle though, it would be wrong to label the whole structure based on only some of the atoms. So we have to be clear in the comments this meaning of "AFM", "SFC", etc.
 def determine_magnetic_ordering(
     df: pd.DataFrame,
     magmom_tolerance: float = 1e-12,
     total_magnetic_moment_tolerance: float = 1e-12,
 ) -> str:
     """Determines the magnetic ordering of a structure from the magnetization
-    data in a pandas DataFrame. e.g. 'FM', 'AFM', 'FiM', 'NM', 'SF'
+    data in a pandas DataFrame. e.g. 'FM', 'AFM', 'FiM', 'NM', 'SF'.
 
     Args:
-        df (pandas DataFrame): a pandas DataFrame containing the magnetization data
+        df (pandas DataFrame): a pandas DataFrame containing the magnetization data.
         magmom_tolerance (float, optional): the tolerance for the total magnetic moment on each atom to be considered zero.
         Total_magmom_tolerance (float, optional) the tolerance for the sum of the total magnentic moments for each atom.
         Defaults to 1e-12 to handle floating point errors.
 
     Returns:
-        The magnetic ordering of the structure
+        str: the magnetic ordering of the structure.
     """
 
     if (np.isclose(df["tot"], 0, atol=magmom_tolerance)).all():
@@ -51,17 +48,18 @@ def determine_magnetic_ordering(
 
 
 def get_magnetic_structure(poscar: str, outcar: str) -> Structure:
-    """Combines the magmom data from the outcar with the structure from the poscar
-    to return a pymatgen magnetic Structures object (e.g. Structures with
+    """Combines the magmom data from the OUTCAR file with the structure from the POSCAR file
+    to return a pymatgen magnetic structures object (e.g. structures with
     associated magmom tags).
 
     Args:
-        poscar (str): name of the POSCAR file
-        outcar (str): name of the OUTCAR file
+        poscar (str): path to the POSCAR file.
+        outcar (str): path to the OUTCAR file.
 
     Returns:
-        Structure: pymatgen Structure object with magmom tags
+        Structure: pymatgen Structure object with magmom tags.
     """
+
     structure = Structure.from_file(poscar)
     mag_data = extract_tot_mag_data(outcar, poscar)
     structure.add_site_property("magmom", mag_data["tot"])
