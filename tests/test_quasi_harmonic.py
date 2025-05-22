0d56cc63f96ada0c65f69e82edb4c1830557d51a
nhew1994 <nigelhew94@gmail.com>
2025-05-22 14:08:42 -0400

    ENH: add tests for QuasiHarmonic class

---
+++ tests/test_quasi_harmonic.py
@@ -0,0 +1,133 @@
+"""
+Tests for the QuasiHarmonic class.
+"""
+
+# Standard library imports
+import os
+import json
+
+# Third-party library imports
+import pytest
+import numpy as np
+
+# DFTTK imports
+from dfttk.eos.functions import BM4_equation
+from dfttk.quasi_harmonic import QuasiHarmonic
+from dfttk.config import Configuration
+
+number_of_atoms = 4
+volumes = np.linspace(0.98 * 60, 1.02 * 74, 1000)
+temperatures = np.arange(0, 1010, 10)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config_Al_path = os.path.join(current_dir, "vasp_data/Al/config_Al")
+
+config_Al = Configuration(config_Al_path, "config_Al")
+config_Al.process_ev_curve()
+config_Al.process_debye(scaling_factor=0.617, gruneisen_x=2 / 3, temperatures=temperatures)
+config_Al.process_thermal_electronic(temperatures, order=1)
+
+qha = QuasiHarmonic(number_of_atoms, volumes, temperatures)
+
+a = config_Al.ev_curve.eos_parameters["a"]
+b = config_Al.ev_curve.eos_parameters["b"]
+c = config_Al.ev_curve.eos_parameters["c"]
+d = config_Al.ev_curve.eos_parameters["d"]
+
+energy_eos = BM4_equation(volumes, a, b, c, d)
+vibrational_helmholtz_energy = config_Al.debye.helmholtz_energies
+vibrational_entropy = config_Al.debye.entropies
+vibrational_heat_capacity = config_Al.debye.heat_capacities
+electronic_helmholtz_energy = np.vstack(config_Al.thermal_electronic.f_el_fit)
+electronic_entropy = np.vstack(config_Al.thermal_electronic.s_el_fit)
+electronic_heat_capacity = np.vstack(config_Al.thermal_electronic.cv_el_fit)
+
+properties = ("helmholtz_energy", "entropy", "heat_capacity", "helmholtz_energy_pv", "V0", "G0", "S0", "H0", "B", "BP", "CTE", "LCTE", "Cp")
+
+
+def test_QuasiHarmonic():
+    """Test QuasiHarmonic results against reference data."""
+    qha.process(
+        "debye",
+        energy_eos,
+        vibrational_helmholtz_energy,
+        vibrational_entropy,
+        vibrational_heat_capacity,
+    )
+    qha.process(
+        "debye_thermal_electronic",
+        energy_eos,
+        vibrational_helmholtz_energy,
+        vibrational_entropy,
+        vibrational_heat_capacity,
+        electronic_helmholtz_energy,
+        electronic_entropy,
+        electronic_heat_capacity,
+    )
+
+    assert qha.number_of_atoms == number_of_atoms
+    assert np.allclose(qha.volumes, volumes)
+    assert np.allclose(qha.temperatures, temperatures)
+
+    files_and_attributes = [
+        ("test_quasi_harmonic_data/qha_debye.json", "debye"),
+        ("test_quasi_harmonic_data/qha_debye_thermal_electronic.json", "debye_thermal_electronic"),
+    ]
+
+    methods_copy = qha.methods
+    for filename, attribute in files_and_attributes:
+        with open(os.path.join(current_dir, filename), "r") as f:
+            expected_data = json.load(f)
+
+        for prop in properties:
+            if prop == "helmholtz_energy":
+                expected_consts = expected_data[prop]["eos_constants"]
+                actual_consts = methods_copy[attribute][prop]["eos_constants"]
+
+                assert expected_consts["eos_name"] == actual_consts["eos_name"]
+                expected_consts.pop("eos_name", None)
+                actual_consts.pop("eos_name", None)
+
+                expected_vals = np.array(expected_data[prop]["values"])
+                actual_vals = methods_copy[attribute][prop]["values"]
+                assert np.allclose(expected_vals, actual_vals, rtol=2e-2), f"Expected {expected_vals}, but got {actual_vals} with tolerance 2e-2"
+
+            elif prop == "helmholtz_energy_pv":
+                expected_consts = expected_data["0_GPa"][prop]["eos_constants"]
+                actual_consts = methods_copy[attribute]["0_GPa"][prop]["eos_constants"]
+
+                expected_vals = np.array(expected_data["0_GPa"][prop]["values"])
+                actual_vals = methods_copy[attribute]["0_GPa"][prop]["values"]
+                assert np.allclose(expected_vals, actual_vals, rtol=2e-2), f"Expected {expected_vals}, but got {actual_vals} with tolerance 2e-2"
+
+                assert expected_consts["eos_name"] == actual_consts["eos_name"]
+                expected_consts.pop("eos_name", None)
+                actual_consts.pop("eos_name", None)
+
+            elif prop in ("entropy", "heat_capacity"):
+                expected_consts = expected_data[prop]["poly_coeffs"]
+                actual_consts = methods_copy[attribute][prop]["poly_coeffs"]
+
+                expected_vals = np.array(expected_data[prop]["values"])
+                actual_vals = methods_copy[attribute][prop]["values"]
+                assert np.allclose(expected_vals, actual_vals, rtol=2e-2), f"Expected {expected_vals}, but got {actual_vals} with tolerance 2e-2"
+
+            elif prop in ("V0", "G0", "S0", "H0", "B", "BP", "CTE", "LCTE", "Cp"):
+                expected_arr = np.array(expected_data["0_GPa"][prop])
+                actual_arr = methods_copy[attribute]["0_GPa"][prop]
+                assert np.allclose(expected_arr, actual_arr, rtol=2e-2), f"Expected {expected_arr}, but got {actual_arr} with tolerance 2e-2"
+
+            # If the property has temperature-dependent coefficients, compare them
+            if prop in ("helmholtz_energy", "helmholtz_energy_pv", "entropy", "heat_capacity"):
+                for temp, expected_values in expected_consts.items():
+                    actual_values = actual_consts[temp]
+                    if prop in ("helmholtz_energy", "helmholtz_energy_pv"):
+                        for expected, actual in zip(expected_values.values(), actual_values.values()):
+                            assert np.allclose(expected, actual, rtol=2e-2), f"Expected {expected}, but got {actual} with tolerance 2e-2"
+                    elif prop in ("entropy", "heat_capacity"):
+                        for expected, actual in zip(expected_values, actual_values):
+                            assert np.allclose(expected, actual, rtol=2e-2), f"Expected {expected}, but got {actual} with tolerance 2e-2"
+
+
+if __name__ == "__main__":
+    pytest.main()
