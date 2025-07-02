65c7184d43754dcaac9b64a0571d6df7b8a5aa98
nhew1994 <nigelhew94@gmail.com>
2025-07-02 14:04:02 -0400

    FIX: fix vasp_cmd argument assignment

--- tests/test_quasi_harmonic.py
+++ tests/test_quasi_harmonic.py
@@ -20,10 +20,10 @@ volumes = np.linspace(0.98 * 60, 1.02 * 74, 1000)
 temperatures = np.arange(0, 1010, 10)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-vasp_cmd = ["mpirun", "/opt/packages/VASP/VASP6/6.4.3/ONEAPI/vasp_std"]
-config_Al_path = os.path.join(current_dir, "vasp_data/Al/config_Al", vasp_cmd)
+config_Al_path = os.path.join(current_dir, "vasp_data/Al/config_Al")
 
-config_Al = Configuration(config_Al_path, "config_Al")
+vasp_cmd = ["mpirun", "/opt/packages/VASP/VASP6/6.4.3/ONEAPI/vasp_std"]
+config_Al = Configuration(config_Al_path, "config_Al", vasp_cmd=vasp_cmd)
 config_Al.process_ev_curve()
 config_Al.process_debye(scaling_factor=0.617, gruneisen_x=2 / 3, temperatures=temperatures)
 config_Al.process_thermal_electronic(temperatures, order=1)
