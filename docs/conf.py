2be982a9b281ae697f3ddf73a928c95362e3c5b5
nhew1994 <nigelhew94@gmail.com>
2026-01-09 21:34:42 -0500

    MAINT: update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,12 +41,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for napoleon ----------------------------------------------------
 napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_use_param = False
-napoleon_use_rtype = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
-napoleon_preprocess_types = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
