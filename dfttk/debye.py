cbe2f183ba770f69dd31e32518dd7485c7c82af9
nhew1994 <nigelhew94@gmail.com>
2026-02-09 12:59:24 -0500

    MAINT: reorganize DebyeGruneisen methods

--- dfttk/debye.py
+++ dfttk/debye.py
@@ -34,145 +34,6 @@ class DebyeGruneisen:
         self.entropies: np.ndarray = None
         self.heat_capacities: np.ndarray = None
 
-    def calculate_gruneisen_parameter(self) -> float:
-        """Calculates the Grüneisen parameter (gamma).
-
-        Returns: Grüneisen parameter (gamma).
-        """
-        return (1 + self.BP) / 2 - self.gruneisen_x
-
-    def calculate_debye_temperatures(self, gruneisen_parameter: float) -> np.ndarray:
-        """Compute the Debye temperature for each volume.
-
-        Args:
-            gruneisen_parameter: Grüneisen parameter (gamma).
-
-        Returns: Debye temperatures for each volume in K.
-        """
-
-        # Compute prefactor using physical constants
-        A = (6 * np.pi**2) ** (1 / 3) * HBAR / BOLTZMANN_CONSTANT
-
-        # Convert units for bulk modulus, volume, and atomic mass
-        B = self.B * 1e12  # GPa to g/(ms^2)
-        V0 = self.V0 * 1e-30  # Å^3 to m^3
-        volumes = self.volumes * 1e-30  # Å^3 to m^3
-        atomic_mass = self.atomic_mass * 1.66054e-24  # u to g
-
-        # Debye temperature formula from the Debye-Grüneisen model
-        debye_temperatures = self.scaling_factor * A * V0 ** (1 / 6) * (B / atomic_mass) ** 0.5 * (V0 / volumes) ** gruneisen_parameter
-        return debye_temperatures
-
-    def calculate_debye_integral_n3(self, x_array: np.ndarray) -> np.ndarray:
-        """Calculate the Debye integral of order 3 for an array of upper limits.
-
-        For each value ``x`` in ``x_array``, this computes:
-
-        .. math::
-
-            D(x) = \\frac{3}{x^3} \\int_0^x \\frac{t^3}{e^t - 1} \\, dt
-
-        Args:
-            x_array: Array of upper integration limits, where each value is the Debye temperature
-                divided by the temperature. The Debye temperature is fixed for a given volume,
-                and the temperature is varied.
-
-        Returns:
-            Array of Debye integrals of order 3 corresponding to each value in x_array.
-        """
-
-        debye_integrals = np.zeros_like(x_array, dtype=float)
-        for i, x in enumerate(x_array):
-            factor = 3.0 / x**3
-            integral, _ = quad(lambda t: t**3 / (np.exp(t) - 1.0), 0, x)
-            debye_integrals[i] = integral * factor
-        return debye_integrals
-
-    def calculate_entropies(self, debye_temperature: float) -> np.ndarray:
-        """Calculate the vibrational entropy.
-
-        Args:
-            debye_temperature: Debye temperature in K for a given volume.
-
-        Returns: Array of vibrational entropy values in eV/K for each temperature.
-        """
-
-        # Masks for zero and nonzero temperatures
-        zero_temp_mask = self.temperatures == 0
-        non_zero_temp_mask = self.temperatures > 0
-
-        entropies = np.zeros_like(self.temperatures)
-        x_array = np.zeros_like(self.temperatures)
-        debye_integrals = np.zeros_like(self.temperatures)
-
-        # Entropy at T = 0 K is zero
-        entropies[zero_temp_mask] = 0
-
-        # Calculate x = theta_D / T for nonzero T
-        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
-        debye_integrals[non_zero_temp_mask] = self.calculate_debye_integral_n3(x_array[non_zero_temp_mask])
-
-        # Debye entropy formula
-        entropies[non_zero_temp_mask] = 3 * self.number_of_atoms * BOLTZMANN_CONSTANT * (4 / 3 * debye_integrals[non_zero_temp_mask] - np.log(1 - np.exp(-x_array[non_zero_temp_mask])))
-        return entropies
-
-    def calculate_helmholtz_energies(self, debye_temperature: float) -> np.ndarray:
-        """Calculates the vibrational Helmholtz energy.
-
-        Args:
-            debye_temperature: Debye temperature in K for a given volume.
-
-        Returns: Array of vibrational Helmholtz energy values in eV for each temperature.
-        """
-
-        zero_temp_mask = self.temperatures == 0
-        non_zero_temp_mask = self.temperatures > 0
-
-        helmholtz_energies = np.zeros_like(self.temperatures)
-        x_array = np.zeros_like(self.temperatures)
-        debye_integrals = np.zeros_like(self.temperatures)
-
-        # Zero point energy at T = 0 K
-        helmholtz_energies[zero_temp_mask] = self.number_of_atoms * (9 / 8 * BOLTZMANN_CONSTANT * debye_temperature)
-        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
-        debye_integrals[non_zero_temp_mask] = self.calculate_debye_integral_n3(x_array[non_zero_temp_mask])
-
-        # Debye Helmholtz energy formula
-        helmholtz_energies[non_zero_temp_mask] = self.number_of_atoms * (9 / 8 * BOLTZMANN_CONSTANT * debye_temperature + BOLTZMANN_CONSTANT * self.temperatures[non_zero_temp_mask] * (3 * np.log(1 - np.exp(-x_array[non_zero_temp_mask])) - debye_integrals[non_zero_temp_mask]))
-        return helmholtz_energies
-
-    def calculate_heat_capacities(self, debye_temperature: float) -> np.ndarray:
-        """Calculates the vibrational heat capacity.
-
-        The integral evaluated is:
-        
-        .. math::
-
-        \frac{3}{x^3} \\int_0^x \\frac{t^4 e^t}{(e^t - 1)^2} \\, dt
-
-        Args:
-            debye_temperature: Debye temperature in K for a given volume.
-
-        Returns: Array of vibrational heat capacity values in eV/K for each temperature.
-        """
-
-        non_zero_temp_mask = self.temperatures > 0
-        x_array = np.zeros_like(self.temperatures)
-        debye_integrals = np.zeros_like(x_array, dtype=float)
-        heat_capacities = np.zeros_like(self.temperatures, dtype=float)
-
-        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
-        for i, x in enumerate(x_array):
-            if x == 0:
-                debye_integrals[i] = 0
-                heat_capacities[i] = 0  # Cv at T = 0 K
-            else:
-                factor = 3.0 / x**3
-                integral, _ = quad(lambda t: (t**4 * np.exp(t)) / (np.exp(t) - 1.0) ** 2, 0, x)
-                debye_integrals[i] = integral * factor
-                heat_capacities[i] = 3 * self.number_of_atoms * BOLTZMANN_CONSTANT * debye_integrals[i]
-        return heat_capacities
-
     def process(
         self,
         number_of_atoms: int,
@@ -225,8 +86,8 @@ class DebyeGruneisen:
 
         # Loop over volumes, calculate properties for all temperatures at each volume
         for i in range(n_vol):
-            entropies_v_t[i, :] = self.calculate_entropies(debye_temperature[i])
             helmholtz_energies_v_t[i, :] = self.calculate_helmholtz_energies(debye_temperature[i])
+            entropies_v_t[i, :] = self.calculate_entropies(debye_temperature[i])
             heat_capacities_v_t[i, :] = self.calculate_heat_capacities(debye_temperature[i])
 
         # Store results as (temperature, volume) arrays
@@ -329,3 +190,142 @@ class DebyeGruneisen:
             plot_format(fig_debye_v, f"Volume (\u212B<sup>3</sup>/{self.number_of_atoms} atoms)", y_label)
 
         return fig_debye_t, fig_debye_v
+    
+    def calculate_gruneisen_parameter(self) -> float:
+        """Calculates the Grüneisen parameter (gamma).
+
+        Returns: Grüneisen parameter (gamma).
+        """
+        return (1 + self.BP) / 2 - self.gruneisen_x
+
+    def calculate_debye_temperatures(self, gruneisen_parameter: float) -> np.ndarray:
+        """Compute the Debye temperature for each volume.
+
+        Args:
+            gruneisen_parameter: Grüneisen parameter (gamma).
+
+        Returns: Debye temperatures for each volume in K.
+        """
+
+        # Compute prefactor using physical constants
+        A = (6 * np.pi**2) ** (1 / 3) * HBAR / BOLTZMANN_CONSTANT
+
+        # Convert units for bulk modulus, volume, and atomic mass
+        B = self.B * 1e12  # GPa to g/(ms^2)
+        V0 = self.V0 * 1e-30  # Å^3 to m^3
+        volumes = self.volumes * 1e-30  # Å^3 to m^3
+        atomic_mass = self.atomic_mass * 1.66054e-24  # u to g
+
+        # Debye temperature formula from the Debye-Grüneisen model
+        debye_temperatures = self.scaling_factor * A * V0 ** (1 / 6) * (B / atomic_mass) ** 0.5 * (V0 / volumes) ** gruneisen_parameter
+        return debye_temperatures
+
+    def calculate_debye_integral_n3(self, x_array: np.ndarray) -> np.ndarray:
+        """Calculate the Debye integral of order 3 for an array of upper limits.
+
+        For each value ``x`` in ``x_array``, this computes:
+
+        .. math::
+
+            D(x) = \\frac{3}{x^3} \\int_0^x \\frac{t^3}{e^t - 1} \\, dt
+
+        Args:
+            x_array: Array of upper integration limits, where each value is the Debye temperature
+                divided by the temperature. The Debye temperature is fixed for a given volume,
+                and the temperature is varied.
+
+        Returns:
+            Array of Debye integrals of order 3 corresponding to each value in x_array.
+        """
+
+        debye_integrals = np.zeros_like(x_array, dtype=float)
+        for i, x in enumerate(x_array):
+            factor = 3.0 / x**3
+            integral, _ = quad(lambda t: t**3 / (np.exp(t) - 1.0), 0, x)
+            debye_integrals[i] = integral * factor
+        return debye_integrals
+
+    def calculate_helmholtz_energies(self, debye_temperature: float) -> np.ndarray:
+        """Calculates the vibrational Helmholtz energy.
+
+        Args:
+            debye_temperature: Debye temperature in K for a given volume.
+
+        Returns: Array of vibrational Helmholtz energy values in eV for each temperature.
+        """
+
+        zero_temp_mask = self.temperatures == 0
+        non_zero_temp_mask = self.temperatures > 0
+
+        helmholtz_energies = np.zeros_like(self.temperatures)
+        x_array = np.zeros_like(self.temperatures)
+        debye_integrals = np.zeros_like(self.temperatures)
+
+        # Zero point energy at T = 0 K
+        helmholtz_energies[zero_temp_mask] = self.number_of_atoms * (9 / 8 * BOLTZMANN_CONSTANT * debye_temperature)
+        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
+        debye_integrals[non_zero_temp_mask] = self.calculate_debye_integral_n3(x_array[non_zero_temp_mask])
+
+        # Debye Helmholtz energy formula
+        helmholtz_energies[non_zero_temp_mask] = self.number_of_atoms * (9 / 8 * BOLTZMANN_CONSTANT * debye_temperature + BOLTZMANN_CONSTANT * self.temperatures[non_zero_temp_mask] * (3 * np.log(1 - np.exp(-x_array[non_zero_temp_mask])) - debye_integrals[non_zero_temp_mask]))
+        return helmholtz_energies
+    
+    def calculate_entropies(self, debye_temperature: float) -> np.ndarray:
+        """Calculate the vibrational entropy.
+
+        Args:
+            debye_temperature: Debye temperature in K for a given volume.
+
+        Returns: Array of vibrational entropy values in eV/K for each temperature.
+        """
+
+        # Masks for zero and nonzero temperatures
+        zero_temp_mask = self.temperatures == 0
+        non_zero_temp_mask = self.temperatures > 0
+
+        entropies = np.zeros_like(self.temperatures)
+        x_array = np.zeros_like(self.temperatures)
+        debye_integrals = np.zeros_like(self.temperatures)
+
+        # Entropy at T = 0 K is zero
+        entropies[zero_temp_mask] = 0
+
+        # Calculate x = theta_D / T for nonzero T
+        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
+        debye_integrals[non_zero_temp_mask] = self.calculate_debye_integral_n3(x_array[non_zero_temp_mask])
+
+        # Debye entropy formula
+        entropies[non_zero_temp_mask] = 3 * self.number_of_atoms * BOLTZMANN_CONSTANT * (4 / 3 * debye_integrals[non_zero_temp_mask] - np.log(1 - np.exp(-x_array[non_zero_temp_mask])))
+        return entropies
+
+    def calculate_heat_capacities(self, debye_temperature: float) -> np.ndarray:
+        """Calculates the vibrational heat capacity.
+
+        The integral evaluated is:
+        
+        .. math::
+
+        \frac{3}{x^3} \\int_0^x \\frac{t^4 e^t}{(e^t - 1)^2} \\, dt
+
+        Args:
+            debye_temperature: Debye temperature in K for a given volume.
+
+        Returns: Array of vibrational heat capacity values in eV/K for each temperature.
+        """
+
+        non_zero_temp_mask = self.temperatures > 0
+        x_array = np.zeros_like(self.temperatures)
+        debye_integrals = np.zeros_like(x_array, dtype=float)
+        heat_capacities = np.zeros_like(self.temperatures, dtype=float)
+
+        x_array[non_zero_temp_mask] = debye_temperature / self.temperatures[non_zero_temp_mask]
+        for i, x in enumerate(x_array):
+            if x == 0:
+                debye_integrals[i] = 0
+                heat_capacities[i] = 0  # Cv at T = 0 K
+            else:
+                factor = 3.0 / x**3
+                integral, _ = quad(lambda t: (t**4 * np.exp(t)) / (np.exp(t) - 1.0) ** 2, 0, x)
+                debye_integrals[i] = integral * factor
+                heat_capacities[i] = 3 * self.number_of_atoms * BOLTZMANN_CONSTANT * debye_integrals[i]
+        return heat_capacities
